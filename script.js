d9430724b45e011c5bdba20e5798d959e4334c45
Vannent <98601749+Vannent@users.noreply.github.com>
2022-03-01 10:18:43 -0600

    Functionality finalize2d

--- script.js
+++ script.js
@@ -1,9 +1,28 @@
-let board = document.querySelector(".board");
-board.style.gridTEmplateColumns = "repeat(16, 1fr)";
-board.style.gridTEmplateRows = "repeat(16, 1fr)";
+let boxesSizePrompt = prompt("What size would you like your boxes to be? Remember that the box has to be smaller than 100px!")
+let boxesSize = parseInt(boxesSizePrompt)
 
-for (let i = 0; i < 256; i++) {
-    let square = document.createElement("div");
-    square.style.backgroundColor = "blue";
-    board.insertAdjacentElement("beforeend", square);
+function Board(size) {
+    let board = document.querySelector(".board");
+    board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+    let amount = size * size;
+
+    for (let i = 0; i < amount; i++) {
+        let square = document.createElement("div");
+       square.style.backgroundColor = "blue";
+        square.addEventListener("mouseover", () => {
+        square.style.backgroundColor = "black"
+        });
+        board.insertAdjacentElement("beforeend", square);
+    }
 }
+
+
+if (boxesSize >= 2 && boxesSize <= 100) {
+    Board(boxesSize)
+} else {
+    console.log("Too many squares");
+    alert("The size is outside of the possible range. Please select a number between 2 and 100!");
+}
+
