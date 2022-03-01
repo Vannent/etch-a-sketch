8f7af5e2604ecd2ad68ce99abf5880968beac67d
Vannent <98601749+Vannent@users.noreply.github.com>
2022-02-28 19:57:56 -0600

    Grid created

---
+++ script.js
@@ -0,0 +1,9 @@
+let board = document.querySelector(".board");
+board.style.gridTEmplateColumns = "repeat(16, 1fr)";
+board.style.gridTEmplateRows = "repeat(16, 1fr)";
+
+for (let i = 0; i < 256; i++) {
+    let square = document.createElement("div");
+    square.style.backgroundColor = "blue";
+    board.insertAdjacentElement("beforeend", square);
+}
